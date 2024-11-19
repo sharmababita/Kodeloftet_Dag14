1ca07c09d0c73a78066d0d860a1bf70b9531b9f7
sharmababita <sharma.089babita@gmail.com>
2024-11-19 10:41:35 +0100

    Pass the value to ID

---
+++ script.js
@@ -0,0 +1,11 @@
+const redSquare = document.querySelector('#redSquare');
+console.log(redSquare);
+
+redSquare.style.height = '100px';
+redSquare.style.width = '100px';
+redSquare.style.backgroundColor = 'blue';
+
+// Oppgave 1: Lag en grønn firkant.
+// // Lage HTML og gi en ID
+// // hENTE ELEMENT TIL js
+// // gI VARIABLE STYLING PROPERTIES
